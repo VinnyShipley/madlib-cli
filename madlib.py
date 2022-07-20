1729cbb782b2d492b78556008a68b395630e719e
Vinny <vinnyshipley@gmail.com>
2022-07-20 13:23:51 -0700

    prompt variables made

---
+++ madlib.py
@@ -0,0 +1,9 @@
+print('Welcome to Madlibs. Please follow the prompts to get a fun lil madlib')
+
+adj_one = input('Please give an adjective: ')
+
+adj_two = input('Please give an adjective: ')
+
+noun_one = input('Please give a noun: ')
+
+print(adj_one + ' ' + adj_two + ' ' + noun_one)
